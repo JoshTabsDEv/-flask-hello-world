48809a969248437f312271023d190e7dcb54e23b
JoshTabsDEv <tabura.joshua@cec.edu.ph>
2025-04-27 13:46:34 +0800

    Landing page for Dionar's marriage proposal 💍 v1

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
 import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Ako si Dionar Boyfriend ni Hime"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
